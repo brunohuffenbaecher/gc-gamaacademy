6f99965c2514312f82fafbed238607e3864cd575
brunohuffenbaecher <bruno.henrique56@hotmail.com>
2021-07-24 15:45:16 -0300

    Created 3 pages. Created starting data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Clients from "./pages/Clients/Clients";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import { loadData } from "./startData";
 
 function App() {
-  return <div>Hello World</div>;
+  useEffect(() => {
+    loadData();
+  }, []);
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/clients" component={Clients} />
+          <Route exact path="/products">
+            <Products />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
